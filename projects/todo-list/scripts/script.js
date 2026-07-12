1e2172b5519798950751b1b4d22c35fca877935b
KiriyaGraysen <kiriyaokazaki@gmail.com>
2026-07-12 23:27:10 +0800

    Updated to do list using forEach, arrow function, and addEventListener.

--- projects/todo-list/scripts/script.js
+++ projects/todo-list/scripts/script.js
@@ -3,11 +3,10 @@ const todoList = JSON.parse(localStorage.getItem('todoLists')) || [];
 function renderTodoList() {
   let todoListHTML = '';
   
-  for (let i = 0; i < todoList.length; i++) {
-    // short hand for:
-    // const name = todoList[i].name;
-    // const dueDate = todoList[i].dueDate;
-    const { name, dueDate } = todoList[i];
+  // The first parameter is the value if the current index
+  // The second parameter is the index
+  todoList.forEach((todoObject, index) => {
+    const { name, dueDate } = todoObject;
     const html = `
     <div>
       ${name}
@@ -15,15 +14,33 @@ function renderTodoList() {
     <div>
       ${dueDate}
     </div>
-    <button onclick="deleteTodo(${i})" class="delete-btn">Delete</button>
+    <button class="delete-btn">Delete</button>
     `;
     todoListHTML += html;
-  }
+  });
   
   document.querySelector('.todo-list').innerHTML = todoListHTML;
+  
+  // Since we used the querySelectorAll to target all the delete button in the page it became a list. The problem is, the deleteTodo function has a parameter index to target and delete the specific item on TodoList using the corresponding delete button. To solve that, we need to loop through the list of button to get the index and use it for deleteTodo function.
+  document.querySelectorAll('.delete-btn')
+    .forEach((deleteBtn, index) => {
+      deleteBtn.addEventListener('click', () => {
+        deleteTodo(index);
+      });
+    });
 }
 
 renderTodoList();
+
+document.querySelector('.add-btn').addEventListener('click', () => {
+  addTodo();
+});
+
+document.querySelector('.name-input').addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    addTodo();
+  }
+});
 
 function addTodo() {
   const nameInput = document.querySelector('.name-input');
