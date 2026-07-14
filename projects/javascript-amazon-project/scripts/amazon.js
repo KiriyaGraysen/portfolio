118308e781ddf44f2310cdc5675f7a7d60553ac6
KiriyaGraysen <kiriyaokazaki@gmail.com>
2026-07-14 11:59:03 +0800

    Added an Added message when clicking add to cart that shows only for 2 secs

--- projects/javascript-amazon-project/scripts/amazon.js
+++ projects/javascript-amazon-project/scripts/amazon.js
@@ -25,7 +25,7 @@ products.forEach((product) => {
       </div>
 
       <div class="product-quantity-container">
-        <select>
+        <select class="quantity-selector-${product.id}">
           <option selected value="1">1</option>
           <option value="2">2</option>
           <option value="3">3</option>
@@ -41,12 +41,12 @@ products.forEach((product) => {
 
       <div class="product-spacer"></div>
 
-      <div class="added-to-cart">
+      <div class="added-to-cart added-to-cart-${product.id}">
         <img src="images/icons/checkmark.png">
         Added
       </div>
 
-      <button class="add-to-cart-button button-primary">
+      <button class="add-to-cart-button button-primary" data-product-id="${product.id}">
         Add to Cart
       </button>
     </div>
@@ -54,3 +54,56 @@ products.forEach((product) => {
 });
 
 document.querySelector('.products-grid').innerHTML = productsHTML;
+
+let timeoutId;
+document.querySelectorAll('.add-to-cart-button').forEach((button) => {
+  button.addEventListener('click', () => {
+    const { productId } = button.dataset;
+    const quantity = Number(document.querySelector(`.quantity-selector-${productId}`).value);
+    
+    // check if there's already existing productId to cart lis
+    let matchingItem;
+    cart.forEach((item) => {
+      if (productId === item.productId) {
+        matchingItem = item;
+      }
+    });
+    
+    // add 1 quantity if there's a match
+    if (matchingItem) {
+      matchingItem.quantity += quantity;
+    
+    // add cart object if no match
+    } else {
+      cart.push({
+        productId,
+        quantity
+      });
+    }
+    
+    let cartQuantity = 0;
+    
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+    
+    document.querySelector('.cart-quantity').innerHTML = String(cartQuantity);
+    const addedToCart = document.querySelector(`.added-to-cart-${productId}`);
+    
+    if (!addedToCart.classList.contains('added')) {
+      addedToCart.classList.add('added');
+      
+      timeoutId = setTimeout(() => {
+        addedToCart.classList.remove('added');
+      }, 2000);
+    } else {
+      clearTimeout(timeoutId);
+      
+      addedToCart.classList.add('added');
+      
+      timeoutId = setTimeout(() => {
+        addedToCart.classList.remove('added');
+      }, 2000);
+    }
+  });
+});
